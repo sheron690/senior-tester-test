29712965fc054434ca2b4f1b0cb4278db1836306
sheron690 <67943225+sheron690@users.noreply.github.com>
2025-05-11 22:47:12 +0100

    Update DriverFactory.java

--- automation/src/test/java/utils/DriverFactory.java
+++ automation/src/test/java/utils/DriverFactory.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+public class DriverFactory {
+
     public static WebDriver createDriver() {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
